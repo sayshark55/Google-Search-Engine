292545a7b733dab8bc36bf9232699d3be0946297
SAYSHARK55 <32270549+sayshark55@users.noreply.github.com>
2022-06-22 12:20:31 +0530

    A Simple Google Search Engine
    A Simple Google Search Engine made with CSS, HTML, and JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+function searchBtnClicked(){
+  var searchData = document.getElementById("searchbar").value;
+  window.location.href = 'https://www.google.com/search?q='+searchData;
+}
+function luckyBtnClicked(){
+  window.location.href ="https://www.google.com/doodles";
+}
